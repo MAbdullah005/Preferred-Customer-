5d702790807de87d2669e91cb3277e8c113c145d
Abdullah Ali <abdullahaliofc@gmail.com>
2024-11-02 14:45:50 -0700

    Create persondata.cs

---
+++ persondata.cs
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+
+class PersonData {
+private:
+    string lastName;
+    string firstName;
+    string address;
+    string city;
+    string state;
+    string zip;
+    string phone;
+
+public:
+    string getLastName() const 
+    { 
+        return lastName; 
+    }
+    string getFirstName() const 
+    { 
+        return firstName; 
+    }
+    string getAddress() const 
+    { 
+        return address; 
+    }
+    string getCity() const 
+    { 
+        return city; 
+    }
+    string getState() const 
+    { 
+        return state; 
+    }
+    string getZip() const 
+    { 
+        return zip; 
+    }
+    string getPhone() const 
+    { 
+        return phone; 
+    }
+    void setLastName(const string &ln) 
+    { 
+        lastName = ln; 
+    }
+    void setFirstName(const string &fn) 
+    { 
+        firstName = fn; 
+    }
+    void setAddress(const string &addr) 
+    { 
+        address = addr; 
+    }
+    void setCity(const string &c) 
+    { 
+        city = c; 
+    }
+    void setState(const string &s) 
+    { 
+        state = s; 
+    }
+    void setZip(const string &z) 
+    { 
+        zip = z; 
+    }
+    void setPhone(const string &p) 
+    { 
+        phone = p; 
+    }
+};
